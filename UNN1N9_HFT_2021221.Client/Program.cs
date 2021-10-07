e3d800e72f8a7d01bfcebf28cdce3cf07b8d0739
akostothz <akostothza@gmail.com>
2021-10-07 16:47:03 +0200

    database created with the right settings

--- UNN1N9_HFT_2021221.Client/Program.cs
+++ UNN1N9_HFT_2021221.Client/Program.cs
@@ -4,9 +4,9 @@ namespace UNN1N9_HFT_2021221.Client
 {
     class Program
     {
+        // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MyDatabase.mdf;Integrated Security=True
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
