93227c911d095b1d518e0ce09600f573d1102c8f
akostothz <akostothza@gmail.com>
2021-10-15 00:41:43 +0200

    Created DbSets in DbContext.

--- UNN1N9_HFT_2021221.Data/MusicDbContext.cs
+++ UNN1N9_HFT_2021221.Data/MusicDbContext.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UNN1N9_HFT_2021221.Models;
 
 namespace UNN1N9_HFT_2021221.Data
 {
     public class MusicDbContext : DbContext
     {
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Song> Songs { get; set; }
         public MusicDbContext()
         {
             this.Database.EnsureCreated();
