57933514c4992471e335ff39ede58dc3c0c1dfb4
akostothz <akostothza@gmail.com>
2021-10-15 00:36:48 +0200

    'SONGS' table's structure has been created.

--- UNN1N9_HFT_2021221.Models/Song.cs
+++ UNN1N9_HFT_2021221.Models/Song.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UNN1N9_HFT_2021221.Models
 {
+    [Table("SONGS")]
     public class Song
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public int SongID { get; set; }
+
+        [Required]
+        public int PerformancerID { get; set; }
+
+        public int? AlbumID { get; set; }
+        
+        [Required]
+        public string SongName { get; set; }
+
+        [MaxLength(3)]
+        [Required]
+        public int Length { get; set; }
+
+        [Required]
+        public string Style { get; set; }
+
+        [NotMapped]
+        public virtual Artist Artist { get; set; }
+
+        [NotMapped]
+        public virtual Album Album { get; set; }
+
+        [NotMapped]
+        public string AllData => $"[{SongID}]: {SongName} BY ID OF {PerformancerID} >> (LENGTH: {Length}, STYLE: {Style})";
     }
 }
