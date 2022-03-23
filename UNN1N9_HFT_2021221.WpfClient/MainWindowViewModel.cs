b61c5da3597c2bed0fe0fbbd25fba29232c7282b
akostothz <akostothza@gmail.com>
2022-03-23 16:01:34 +0100

    RelayCommands

--- UNN1N9_HFT_2021221.WpfClient/MainWindowViewModel.cs
+++ UNN1N9_HFT_2021221.WpfClient/MainWindowViewModel.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using UNN1N9_HFT_2021221.Models;
 
 namespace UNN1N9_HFT_2021221.WpfClient
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
         public static bool IsInDesignMode
         {
@@ -21,12 +24,59 @@ namespace UNN1N9_HFT_2021221.WpfClient
         }
         public RestCollection<Song> Songs { get; set; }
 
+        private Song selectedSong;
+
+        public Song SelectedSong
+        {
+            get { return selectedSong; }
+            set 
+            {
+                selectedSong = new Song()
+                {
+                    SongName = value.SongName,
+                    SongID = value.SongID,
+                    Style = value.Style,
+                    Length = value.Length,
+                    IsExplicit = value.IsExplicit,
+                    IsLoveSong = value.IsLoveSong
+                };
+                OnPropertyChanged();
+                (DeleteSongCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+
+        public ICommand CreateSongCommand { get; set; }
+        public ICommand DeleteSongCommand { get; set; }
+        public ICommand UpdateSongCommand { get; set; }
         public MainWindowViewModel()
         {
             if (!IsInDesignMode)
             {
                 Songs = new RestCollection<Song>("http://localhost:35739", "song");
-            }           
+
+                CreateSongCommand = new RelayCommand(() =>
+                {
+                    Songs.Add(new Song()
+                    {
+                        SongName = "Softcore"
+                    });
+                });
+
+                UpdateSongCommand = new RelayCommand(() =>
+                {
+
+                });
+
+                DeleteSongCommand = new RelayCommand(() =>
+                {
+                    Songs.Delete(SelectedSong.SongID);
+                },
+                () =>
+                {
+                    return SelectedSong != null;
+                });
+            }          
         }
     }
 }
